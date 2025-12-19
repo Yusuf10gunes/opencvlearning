7cef14e0d211501d0612c9febe58ccdab9c73715
Yusuf10gunes <yusufgunes1278@gmail.com>
2025-12-19 22:20:02 +0300

    Resimleri birleştirme

--- opencv.py
+++ opencv.py
@@ -196,8 +196,36 @@ import matplotlib.pyplot as plt
 import numpy as np 
 
  
-img1 = cv.imread("img1.JPG")
-img2 = cv.imread("img2.JPG")
+img1 = cv.imread("img1.jpg")
+img1 = cv.cvtColor(img1,cv.COLOR_BGR2RGB)
+img2 = cv.imread("img2.jpg")
+img2 = cv.cvtColor(img2,cv.COLOR_BGR2RGB) 
+
+
+
 plt.figure()
 plt.imshow(img1)
+
+plt.figure()
+plt.imshow(img2)    
+plt.show()
+print(img1.shape)
+print(img2.shape)
+img1 = cv.resize(img1,(600,600))
+img2 = cv.resize(img2,(600,600))
+print(img1.shape)
+print(img2.shape)
+
+
+plt.figure()
+plt.imshow(img1)
+
+plt.figure()
 plt.imshow(img2)
+plt.show()
+
+# karıştırılmış resim = alpha*img1 + beta*img2
+blended_picture = cv.addWeighted(src1=img1,alpha=0.5,src2=img2,beta = 0.5,gamma=0)
+plt.figure()
+plt.imshow(blended_picture)
+plt.show()
